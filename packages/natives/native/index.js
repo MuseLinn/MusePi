34fe170a77d85ec44289df68d73d99812dbdcc6a
MuseLinn <40310493+MuseLinn@users.noreply.github.com>
2026-09-07 11:16:26 +0800

    chore: bump version to 0.4.19

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -29,7 +29,7 @@ export const Shell = nativeBindings.Shell;
 
 // functions
 export const __ompInstallTokioRuntime = nativeBindings.__ompInstallTokioRuntime;
-export const __piNativesV0_4_18 = nativeBindings.__piNativesV0_4_18;
+export const __piNativesV0_4_19 = nativeBindings.__piNativesV0_4_19;
 export const astEdit = nativeBindings.astEdit;
 export const astGrep = nativeBindings.astGrep;
 export const astMatch = nativeBindings.astMatch;
