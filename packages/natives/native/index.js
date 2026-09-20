be0db918998477bee9eb58b04905f7af594e9fda
MuseLinn <universe_yuan@icloud.com>
2026-09-21 01:09:06 +0800

    chore: bump version to 0.4.35

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -29,7 +29,7 @@ export const Shell = nativeBindings.Shell;
 
 // functions
 export const __ompInstallTokioRuntime = nativeBindings.__ompInstallTokioRuntime;
-export const __piNativesV0_4_34 = nativeBindings.__piNativesV0_4_34;
+export const __piNativesV0_4_35 = nativeBindings.__piNativesV0_4_35;
 export const astEdit = nativeBindings.astEdit;
 export const astGrep = nativeBindings.astGrep;
 export const astMatch = nativeBindings.astMatch;
